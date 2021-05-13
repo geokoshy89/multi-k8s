87e5a7d8fb7a51e8d6a1898e9966db99e08a57d7
unknown <geo.koshy89@gmail.com>
2021-05-13 18:38:14 +0000

    Added deployment scripts

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,17 @@
-docker build -t geokoshy89/multi-client -f ./client/Dockerfile ./client
-dokcer build -t geokoshy89/multi-server -f ./server/Dockerfile ./server
-docker build -t geokoshy89/-multi-worker -f ./worker/Dockerfile ./worker
-docker push geokoshy89/multi-client
-docker push geokoshy89/multi-server
-docker push geokoshy89/mult-worker
+docker build -t geokoshy89/multi-client:latest -t geokoshy89/multi-client:$SHA -f ./client/Dockerfile ./client
+dokcer build -t geokoshy89/multi-server:latest -t geokoshy89/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t geokoshy89/multi-worker:latest -t geokoshy89/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+
+docker push geokoshy89/multi-client:latest
+docker push geokoshy89/multi-server:latest
+docker push geokoshy89/mult-worker:latest
+
+docker push geokoshy89/multi-client:$SHA
+docker push geokoshy89/multi-server:$SHA
+docker push geokoshy89/mult-worker:$SHA
+
 kubectl apply -f k8s
-kubectl set image deployments/server-deployment server=geokoshy89/multi-server
+
+kubectl set image deployments/server-deployment server=geokoshy89/multi-server:$SHA
+kubectl set image deployments/client-deployment client=geokoshy89/multi-client:$SHA
+kubectl set image deployments/worker-deployment worker=geokoshy89/multi-worker:$SHA
